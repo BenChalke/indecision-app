06256e8e7fa081d2c02c945e64d87546f96d72de
Benjamin Chalke <benjamin.chalke@intygrate.com>
2020-03-16 00:33:13 +1100

    Moved components into own files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log('app.js is running');
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisonApp from "./components/IndecisionApp";
+
+ReactDOM.render(<IndecisonApp />, document.getElementById("app"));
