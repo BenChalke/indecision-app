60972e85442b4eb2f58f017528b0972687aabcb9
Benjamin Chalke <benjamin.chalke@intygrate.com>
2020-03-16 15:12:15 +1100

    Added SCSS support

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import IndecisonApp from "./components/IndecisionApp";
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisonApp />, document.getElementById("app"));
